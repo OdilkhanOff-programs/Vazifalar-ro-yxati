7bf27817d1ff17dc0d280835c53b33889454a50f
Odilkhan Sayfullayev <sayfullayevodilxon72@gmail.com>
2024-12-22 21:36:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,9 +6,47 @@ function handleClick() {
     const taskList = document.getElementById("tasks-list");
     const newTask = document.createElement("div");
     newTask.className = "task-item";
-    newTask.textContent = taskText;
 
+    
+    const taskContent = document.createElement("span");
+    taskContent.textContent = taskText;
+
+
+    const buttonsParent = document.createElement("div");
+    buttonsParent.className = "buttons__parent";
+
+    
+    const editButton = document.createElement("button");
+    editButton.textContent = "Tahrirlash";
+    editButton.className = "task-edit-btn";
+    editButton.onclick = function () {
+      const newTaskText = prompt(
+        "Yangi vazifani kiriting:",
+        taskContent.textContent
+      );
+      if (newTaskText) {
+        taskContent.textContent = newTaskText.trim();
+      }
+    };
+
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "O'chirish";
+    deleteButton.className = "task-delete-btn";
+    deleteButton.onclick = function () {
+      taskList.removeChild(newTask);
+    };
+
+   
+    buttonsParent.appendChild(editButton);
+    buttonsParent.appendChild(deleteButton);
+
+   
+    newTask.appendChild(taskContent);
+    newTask.appendChild(buttonsParent);
     taskList.appendChild(newTask);
+
+    
     taskInput.value = "";
   }
 }
